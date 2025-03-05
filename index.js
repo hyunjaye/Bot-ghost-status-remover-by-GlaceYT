571479debc1f2487eea2545cf5cc3a923ec0d907
hyunjaye <omalayjay@gmail.com>
2025-03-06 06:55:57 +0800

    Update index.jss

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = [".gg/Hometown-ChitChat
+", ".gg/Hometown-ChitChat"];
+
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
